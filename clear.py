4d147549c5f195e0553709bcf16f885fc66b8ce4
SENAI4LIFE <brunoalexandersenai@gmail.com>
2026-03-05 11:34:58 -0300

    Update and rename clear_commit.py to clear.py

--- clear.py
+++ clear.py
@@ -10,14 +10,14 @@ def run_command(command, allow_fail=False):
 run_command("git branch -D latest_branch", allow_fail=True)
 run_command("git checkout --orphan latest_branch")
 run_command("git add -A")
-run_command("git commit -m 'Initial commit'")
+run_command("git commit -m 'first commit'")
 run_command("git branch -D main", allow_fail=True)
 run_command("git branch -m main")
 
-print("\nReady to push. This will overwrite the history on GitHub.")
+print("\n type yes to overwrite the history on GitHub.")
 confirm = input("Type 'yes' to force push: ")
 
 if confirm.lower() == 'yes':
     run_command("git push -f origin main")
 else:
-    print("Push cancelled.")
+    print("Cancelled.")
